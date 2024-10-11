3a554fbe5d5da6b333325b5aa5b2193f1f822ba1
Eveline Huiser <eveline.huiser@capgemini.com>
2024-10-11 13:30:31 +0200

    user list and display name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -8,17 +8,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
-  
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
   get imagePath() {
-    return `./assets/users/${this.selectedUser.avatar}`;
+    return `./assets/users/${this.avatar}`;
   }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]
+    this.select.emit(this.id);
   }
 }
