78273e0b0e1425c72f3de0b5b442e454249d79cd
Eveline Huiser <eveline.huiser@capgemini.com>
2024-10-14 10:29:51 +0200

    user active and task complete button adjustments

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,9 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-};
 
 @Component({
   selector: 'app-user',
@@ -18,6 +13,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
